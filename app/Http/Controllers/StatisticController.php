3b104be0e7517ed0d079fd571e68efba73d66d64
Andrew <kuzmichev.2012@mail.ru>
2015-08-09 22:14:19 +0300

    статистика почти работает, остался только лучший бросок, и понять, как передать результаты через js

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -1,27 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Player;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use App\Models\Player;
-class DataPlayer{
+
+class DataPlayer {
 	public $countWin;
+	public $procentWin;
 	public $countLose;
+	public $procentLose;
 	public $games;
+	public $bestResult;
 	public $allScores;
 
-	public function __construct( $countWin, $countLose, $games, $all_scores ){
-		$this->countWin = $countWin;
-		$this->countLose = $countLose;
+
+	public function __construct( $count_win, $count_lose, $games, $all_scores, $best_result ){
+		$this->countWin = $count_win;
+		$this->countLose = $count_lose;
 		$this->games = $games;
+		$this->bestResult = $best_result;
 		$this->allScores = $all_scores;
 	}
 	public function addWins(){
 		$this->countWin++;
 	}
+
 	public function addLose(){
 		$this->countLose++;
 	}
@@ -31,48 +37,84 @@ class DataPlayer{
 	public function addAllScores( $resultScore ){
 		$this->allScores += $resultScore;
 	}
+	static function countProcentWin( $countWin, $games ){
+		return round( ($countWin/$games)*100 );
+	}
+	static function countProcentLose( $countLose, $games ){
+		return round( ($countLose/$games)*100 );
+	}
 }
 
 class StatisticController extends MainController{
 	public $data;
 
 	public function index( Player $playerModel ){//вывод таблицы
-		//$this->data['statistics'] = $playerModel->all();
-		$result = ['Андрей'=>120, 'Вова'=>100, 'Олег'=>32 ];//этот массив мы получим, а уже потом разделим
-		$resultNames = ["Андрей" , "Вова", "Олег"];
-		$resultScores = [120,100,32];
-		$noChangeData =  $this->getPlayer( $playerModel, $resultNames );
+
+		$result = ['Андрей'=>120, 'Максим Петров'=>20 , 'Вова'=>10];//этот массив мы получим, а уже потом раздели
+		$resultThrow = ['Андрей'=>120, 'Максим Петров'=>20 , 'Вова'=>10];//еще один массив, с наилучшими попытками
+
+		foreach( $result as $key => $value ){
+			$testResultNames[] = $key;
+		}
+
+		echo "<pre>".print_r( $testResultNames ,1)."</pre>" ;
+		$resultNames = ['Андрей', "Максим Петров" , "Вова"];
+
 		$objArray = [];
-		$objArray = $this->createPlayersObjects( $noChangeData ); // метод создания массива объектов DataPlayer для хранения и изменения данных выборки
-		echo "<pre>".print_r( $objArray ,1)."</pre>";
-		$objArray = $this->changeBasicFields( $objArray, $result ); //Изменение основных полей объекта (count_win,count_lose,games)
 
-		echo "<pre>".print_r( $objArray ,1)."</pre>";
+		$noChangeData =  $this->getPlayer( $playerModel, $resultNames );
+		$objArray = $this->createPlayersObjects( $noChangeData ); // метод создания массива объектов DataPlayer для хранения и изменения данных выбор
+		$objArray = $this->changeBasicFields( $objArray, $result, $resultNames); //Изменение основных полей объекта (count_win,count_lose,games)
+		echo "<pre>".print_r( $objArray ,1)."</pre>" ;
+		$this->updateFields($noChangeData, $objArray);
 
-		//return view('statistics', $this->data);
+
+		$this->data['statistics'] = $playerModel->orderBy('procent_win',"DESC")->get();
+		return view('statistics', $this->data);
 	}
-	public function getPlayer(Player $playerModel, $resultNames){
-		return $playerModel->whereIn('name', $resultNames)->get();
-	}							//выборка строк для update where name = result['name']
-								// изменение games++, если победитель countWins++, иначе countLose ++;
-								//для всех играющих изменить очки
-	public function changeBasicFields( array $objArray, array $result ){
-		$index = true;
+	private function updateFields( $noChangeData, $objArray ){
+		foreach ($noChangeData as  &$saveData) {
+			foreach ($objArray as $key => $objData) {
+				if( $saveData['name'] == $key ){
+					$saveData->count_win = $objData->countWin;
+					$saveData->procent_win = $objData->procentWin;
+					$saveData->procent_lose = $objData->procentLose;
+					$saveData->count_lose = $objData->countLose;
+					$saveData->best_result = $objData->bestResult;
+					$saveData->games = $objData->games;
+					$saveData->all_scores = $objData->allScores;
+					$saveData->save();
+				}
+			}
+		}
+	}
+	public function changeBasicFields( array $objArray, array $result, array $resultNames){
+
 		foreach( $objArray as $key=> &$value ){
-			if( $index ){
+			if( $key == $resultNames[0] ){
 				$value->addWins();
-				$index = false;
 			}else $value->addLose();
+
+			if( $value->bestResult < $result[$key] ){//лучшая игра
+				$value->bestResult = $result[$key];
+			}
 			$value->addGames();
+			$value->procentWin = DataPlayer::countProcentWin( $value->countWin, $value->games );
+			$value->procentLose = DataPlayer::countProcentLose( $value->countLose, $value->games );
 			$value->addAllScores( $result[$key] );
 		}
 		return $objArray;
 	}
 	public function createPlayersObjects( $noChangeData ){
 		foreach ($noChangeData as $key => $value) {
-			$objArray[ $value['name'] ] = new DataPlayer( $value['count_win'], $value['count_lose'], $value['games'], $value['all_scores'] );
+			$objArray[ $value['name'] ] = new DataPlayer( $value['count_win'], $value['count_lose'], $value['games'],$value['all_scores'], $value['best_result'] );
 		}
 		return $objArray;
 	}
+	public function getPlayer(Player $playerModel, $resultNames){
+		return $playerModel->whereIn('name', $resultNames)->get();
+	}							//выборка строк для update where name = result['name']
+								// изменение games++, если победитель countWins++, иначе countLose ++;
+								//для всех играющих изменить очки
 
 }
--- app/Models/Player.php
+++ app/Models/Player.php
@@ -5,8 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Player extends Model{
-
+    protected $fillable = ['name','count_win','count_lose', 'games','all_scores'];
+    public $timestamps = false;
+    
     public function getPlayerNames(){
         return $this->get()->pluck('name');
     }
+
 }
